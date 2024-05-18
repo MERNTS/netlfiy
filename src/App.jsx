10a4ab3a4272651326b2d34557a8513c293c8cb7
aiXpertLab <aiXpertLab@gmail.com>
2024-05-18 00:13:42 -0400

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,101 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+import './css/App.css'
+import Login from './components/Login'
+import Note from './components/Note'
+import noteService from './components/communication'
+import getDate from './components/GetDate'
+import config from './components/config';
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('Nota: ') 
+  const [userId, setUserId] = useState(null);
+
+  const handleLogin = (userId) => {
+    // Receive userId from the Login component and set it in App.jsx state
+    setUserId(userId);
+  };
+
+  const hook = () => {
+    console.log('effect')
+    noteService.getAll()
+               .then(response => {
+                  console.log('promised fulfileeeed')
+                  setNotes(response.data)
+              })
+  }
+  useEffect(hook, []) 
+
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    const noteObject = {
+      userid: userId,
+      timestamp: getDate(),
+      content: newNote,
+    }
+
+    noteService
+      .create(noteObject)
+      .then(response => {
+        setNotes(notes.concat(response.data))
+        setNewNote('')
+      })
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    axios.post(`${config.baseURL}/login`, { email, password })
+    .then(result => {
+        console.log(result)
+        if(result.data === "Success"){
+            navigate("/home")
+        }else{
+            navigate("/register")
+            alert("You are not registered to this service")
+
+        }
+   
+    })
+    .catch(err => console.log(err))
+}
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Login onLogin={handleLogin} />
+      <div class="container">
+        <div class="chat-body">
+          <ul class="chatbot-message"> 
+            {notes.filter((note) => note.userid === userId)
+              .map(note => 
+              <Note key={note.id} note={note} />
+            )}
+          </ul>
+        </div>
+
+        <div class="chat-input">
+          <form onSubmit={addNote} className="chat-form">
+            
+            <textarea 
+              placeholder="Type a note..."
+              value={newNote}
+              onChange={handleNoteChange}
+              rows={2} // Adjust the number of rows as needed
+            />
+            <button type="submit">Send</button>
+          </form>
+        </div>
+
+        <p className="read-the-docs">The minimalist note-taking app for daily tasks and journaling. Write, remember, forget nothing.</p>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
+// mongodb/github: tctorbao@gmail.com
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './css/index.css'
+
+const notes = [{id: 1,    content: 'Declutter your mind, embrace minimalist note-taking.',    important: true  },]
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App notes = {notes}/>
 )
